1bdc4c8707fd1e130e669c3ad273b5806d450d99
kn478619 <kn478619@ohio.edu>
2021-05-12 19:53:50 -0400

    First C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Kishor Nepal>
+ *        Date: <may 12>
+ * Description: Thus is th first c++ program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
